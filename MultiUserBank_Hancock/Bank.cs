d4dcfb372ae0eb61fa5a2a4be30273d463868ec5
Brittany Hancock <brittany.hancock@student.centralia.edu>
2023-05-08 06:32:58 -0700

    Add project files.

---
+++ MultiUserBank_Hancock/Bank.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiUserBank_Hancock
+{
+    internal class Bank
+    {
+        private decimal _bankBalance;
+        private string[] _username = { "jlennon", "pmccartney", "gharrison", "rstarr" };
+        private string[] _userPassword = { "johnny", "pauly", "georgy", "ringoy" };
+        private decimal[] _userBalance = { 1250, 2500, 3000, 1000 };
+
+        public Bank(decimal bankBalance)
+        {
+            _bankBalance = bankBalance;
+        }
+
+        public void Withdrawal(int i, decimal withdrawal)
+        {
+            if (withdrawal > 500)
+            {
+                withdrawal = 500;
+            }
+
+            if ((_bankBalance - withdrawal) < 0)
+            {
+                withdrawal = withdrawal + (_bankBalance - withdrawal);
+            }
+
+            if ((_userBalance[i] - withdrawal) < 0)
+            {
+                withdrawal = withdrawal + (_userBalance[i] - withdrawal);
+            }
+
+            _userBalance[i] -= withdrawal;
+            _bankBalance -= withdrawal;
+        }
+
+        public void Deposit(int index, decimal deposit)
+        {
+            _userBalance[index] += deposit;
+            _bankBalance += deposit;
+        }
+
+        public string CheckUsername(string entry)
+        {
+            for (int i = 0; i < _username.Length; i++)
+            {
+                if (entry == _username[i])
+                {
+                    return _username[i];
+                }
+            }
+            return "account does not exist";
+        }
+
+        public int UserPassword(string password, string username)
+        {
+
+            int index = -1;
+            for (int i = 0; i < _userPassword.Length; i++)
+            {
+                if (password == _userPassword[i])
+                    return i;
+            }
+            return index;
+        }
+
+        public string GetUsername(int index)
+        {
+            return _username[index];
+        }
+
+        public decimal UserBalance(int index)
+        {
+
+            return _userBalance[index];
+        }
+
+        public decimal BankBalance
+        {
+            get
+            {
+                return _bankBalance;
+            }
+        }
+    }
+}
---
+++ MultiUserBank_Hancock/Program.cs
@@ -0,0 +1,158 @@
+﻿namespace MultiUserBank_Hancock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal balance = 10000;
+            Bank bank = new Bank(10000);
+
+
+            string entry;
+            int index;
+
+            do
+            {
+                MainMenu();
+
+                entry = ValidateUsername();
+
+                index = MatchPassword(entry);
+
+                Submenu(index);
+            } while (true);
+
+            void Message(int i)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("\nWell done, " + bank.GetUsername(index) + " !");
+                }
+                if (i != 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nSuccess! \nNew account balance: " + bank.UserBalance(index).ToString("C"));
+                }
+            }
+
+            void Submenu(int index)
+            {
+                Console.Clear();
+                string option;
+                int i = 0;
+                do
+                {
+                    i++;
+
+                    do
+                    {
+                        if (i == 1)
+                        {
+                            Console.WriteLine("\n" + bank.GetUsername(index));
+                            Console.WriteLine("\nBank's total balance: " + bank.BankBalance.ToString("C"));
+                        }
+                        Console.WriteLine("\nPress: \n1 - to DEPOSIT\n2 - to WITHDRAWAL\n3 - to CHECK BALANCE\n" +
+                            "4 - to LOG OFF");
+                        option = Console.ReadLine();
+                    } while (option != "1" && option != "2" && option != "3" && option != "4");
+
+                    if (option == "1")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\n" + bank.GetUsername(index));
+                        Console.Write("Enter deposit amount: ");
+                        string amount = Console.ReadLine();
+                        bank.Deposit(index, decimal.Parse(amount));
+                        Console.Clear();
+                        Console.WriteLine("\n" + bank.GetUsername(index));
+                        Console.WriteLine("Your current balance: " + bank.UserBalance(index).ToString("C") + "\n");
+                        Console.WriteLine("\nBank's total balance: " + bank.BankBalance.ToString("C"));
+                    }
+                    if (option == "2")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\n" + bank.GetUsername(index));
+                        Console.Write("Enter withdrawal amount: ");
+                        string amount = Console.ReadLine();
+                        bank.Withdrawal(index, decimal.Parse(amount));
+                        Console.Clear();
+                        Console.WriteLine("\n" + bank.GetUsername(index));
+                        Console.WriteLine("Your current balance: " + bank.UserBalance(index).ToString("C") + "\n");
+                        Console.WriteLine("\nBank's total balance: " + bank.BankBalance.ToString("C"));
+                    }
+                    if (option == "3")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\n" + bank.GetUsername(index));
+                        Console.WriteLine("Your current balance: " + bank.UserBalance(index).ToString("C") + "\n");
+                        Console.WriteLine("\nBank's total balance: " + bank.BankBalance.ToString("C"));
+                    }
+                } while (option != "4");
+                Console.Clear();
+                Console.WriteLine("Logging off...");
+                Thread.Sleep(900);
+            }
+
+            string ValidateUsername()
+            {
+                Console.Clear();
+                do
+                {
+                    entry = string.Empty;
+                    Console.Write("\nEnter username: ");
+                    entry = Console.ReadLine();
+                    entry = bank.CheckUsername(entry);
+                } while (entry == "account does not exist");
+                return entry;
+            }
+
+
+
+            int MatchPassword(string username)
+            {
+                string input;
+                int index;
+                do
+                {
+                    Console.Write("Enter password: ");
+                    input = Console.ReadLine();
+                    index = bank.UserPassword(input, username);
+                } while (index == -1);
+
+                return index;
+            }
+
+            void MainMenu()
+            {
+                Console.Clear();
+                string input;
+                int i = -1;
+                do
+                {
+                    i++;
+                    input = string.Empty;
+                    if (i == 0)
+                    {
+                        Console.WriteLine("\nGreetings, esteemed customer !");
+                    }
+                    Console.WriteLine("\nPress:\n1 - to LOG IN\n2 - to EXIT");
+                    input = Console.ReadLine();
+                    if (input == "2")
+                    {
+                        ExitMode();
+                    }
+                } while (input != "1");
+                return;
+            }
+
+            void ExitMode()
+            {
+                Console.Clear();
+                Console.WriteLine("\nIt was our privelege to serve you !");
+                Thread.Sleep(2500);
+                Console.Clear();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
